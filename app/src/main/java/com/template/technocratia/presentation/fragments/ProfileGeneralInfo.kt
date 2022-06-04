2373264ba9d1163af65db91b01025dbade93d33e
EvgenyPetrashko <e.petrashko@innopolis.university>
2022-06-04 22:59:01 +0300

    Merge ProfileGeneralInfo.kt and ProfileAdditionalInfo.kt to one fragment

--- app/src/main/java/com/template/technocratia/presentation/fragments/ProfileGeneralInfo.kt
+++ app/src/main/java/com/template/technocratia/presentation/fragments/ProfileGeneralInfo.kt
@@ -1,9 +1,12 @@
 package com.template.technocratia.presentation.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -18,6 +21,11 @@ class ProfileGeneralInfo : Fragment() {
     private lateinit var userFullName: TextView
     private lateinit var userDateBirth: TextView
     private lateinit var userImgProfile: ImageView
+    private lateinit var userPhoneNumber: TextView
+    private lateinit var userLocation: TextView
+    private lateinit var userCoordinates: TextView
+    private lateinit var userDialBtn: ImageButton
+    private lateinit var userMapBtn: ImageButton
     private val mainActivityViewModel: MainActivityViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +36,20 @@ class ProfileGeneralInfo : Fragment() {
         userFullName = view.findViewById(R.id.profileFullName)
         userDateBirth = view.findViewById(R.id.profileDateBirth)
         userImgProfile = view.findViewById(R.id.profileImg)
+        userPhoneNumber = view.findViewById(R.id.profilePhone)
+        userLocation = view.findViewById(R.id.profileLocation)
+        userCoordinates = view.findViewById(R.id.profileCoordinates)
+
+        userDialBtn = view.findViewById(R.id.profileCallBtn)
+        userDialBtn.setOnClickListener {
+            dialPhoneNumber()
+        }
+
+        userMapBtn = view.findViewById(R.id.profileMapBtn)
+        userMapBtn.setOnClickListener {
+            showMap()
+        }
+
         mainActivityViewModel.users.observe(viewLifecycleOwner) {
             userFullName.text = it.fullName
             userDateBirth.text = it.dateOfBirth
@@ -37,7 +59,28 @@ class ProfileGeneralInfo : Fragment() {
                 .placeholder(R.drawable.ic_launcher_foreground)
                 .error(R.drawable.ic_launcher_foreground)
                 .into(userImgProfile)
+            userPhoneNumber.text = it.phoneNumber
+            userLocation.text = it.location
+            userCoordinates.text = it.coordinates
         }
         return view
+    }
+
+    private fun dialPhoneNumber() {
+        val phoneNumber = mainActivityViewModel.users.value?.phoneNumber
+        if (phoneNumber != null) {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:${phoneNumber}")
+            startActivity(intent)
+        }
+    }
+
+    private fun showMap() {
+        val coordinates = mainActivityViewModel.users.value?.coordinates
+        if (coordinates != null) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("geo:$coordinates")
+            startActivity(intent)
+        }
     }
 }
